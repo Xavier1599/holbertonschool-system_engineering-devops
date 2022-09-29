fbd6f7394e8e74b2a813007f66b1e61227b9cbaa
Xavier <xavierperez1599@gmail.com>
2022-09-29 14:18:25 -0400

    adding task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """function that queries the Reddit API
 and returns the number of subscribers"""
+import json
 import requests
 import sys
-import json
 
 
 def number_of_subscribers(subreddit):
